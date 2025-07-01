8c8c44346f8a2950e38fde1093502964437edcba
LabAsim <lampgit@pm.me>
2025-07-01 19:16:07 +0300

    unit test helper functions

---
+++ tests/testthat/test-helpers.R
@@ -0,0 +1,31 @@
+test_that(
+  desc = "return dashed styles if p-value <0.05, else empty string",
+  code = {
+    expect_identical(
+      add_edge_style(pvals = 0.049),
+      ""
+    )
+    expect_identical(
+      add_edge_style(pvals = 0.05),
+      ""
+    )
+    expect_identical(
+      add_edge_style(pvals = 0.051),
+      " style = dashed "
+    )
+  }
+)
+
+test_that(
+  desc = "create styles based on a list of pvalues",
+  code = {
+    expect_identical(
+      create_edge_styles(pvals = list(0.049, 0.05, 0.051)),
+      c("", "", "")
+    )
+    expect_identical(
+      create_edge_styles(pvals = list(0.049, 0.05, 0.051), edge_styles = T),
+      c("", "", " style = dashed ")
+    )
+  }
+)
