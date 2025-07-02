651de172e0839477e9e17643318915042297b9bf
LabAsim <lampgit@pm.me>
2025-07-03 00:44:37 +0300

    update cache in github actions, update files

---
+++ R/helper.R
@@ -0,0 +1,24 @@
+#' Generates edge styles
+#'
+#' @param pvals a vector of p values
+add_edge_style <- function(pvals) {
+  if (pvals <= 0.05) {
+    line_style <- ""
+  } else {
+    line_style <- " style = dashed "
+  }
+  return(line_style)
+}
+
+#' Creates  edge styles
+#'
+#' @param pvals a vector of p values
+#' @param edge_styles  boolean, sets edge styles based on p-values
+create_edge_styles <- function(pvals, edge_styles = F) {
+  if (edge_styles == T) {
+    edge_styles <- unlist(lapply(X = pvals, FUN = add_edge_style))
+  } else {
+    edge_styles <- rep(x = "", times = length(pvals))
+  }
+  return(edge_styles)
+}
